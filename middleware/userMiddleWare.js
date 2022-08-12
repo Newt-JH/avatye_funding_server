7254540d84039eb2ad0c1da78c83f77ea6f19f72
Newt-JH <flozzang@naver.com>
2022-08-12 14:09:17 +0900

    코드 정리

--- middleware/userMiddleWare.js
+++ middleware/userMiddleWare.js
@@ -4,14 +4,14 @@ const wrapper = wrap.wrapper;
 const secret = 'hwan'
 
 // 토큰 검증
- async function verifyToken(token){
-    try{
-        decodeToken = jwt.verify(token,secret);
+async function verifyToken(token) {
+    try {
+        decodeToken = jwt.verify(token, secret);
         return decodeToken;
-    }catch(err){
-        if(err.name === 'TokenExpiredError'){
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
             return {
-                code:419,
+                code: 419,
                 massage: '토큰이 만료되었습니다.'
             }
         }
