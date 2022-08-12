6f9a88dc17e8b71d2e77faacddcd54ae30d116d8
Newt-JH <flozzang@naver.com>
2022-08-12 11:44:22 +0900

    commit

--- routes/user/anotherUserProfile.js
+++ routes/user/anotherUserProfile.js
@@ -6,17 +6,40 @@ const middle = require('../../middleware/userMiddleWare');
 const wrap = require('../../util/wrapper');
 const wrapper = wrap.wrapper;
 
+// 찜 목록 나타내는 부분 모듈화 처리
+const readHeart = wrapper(async function (req, res, query) {
+    // 토큰 가져오기
+    const toke = req.get('user_token');
+    if (toke !== undefined) {
+        // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
+        const msg = await middle.verifyToken(toke);
+        // 에러 발생 시 res로 에러 반환
+        if (msg.code) {
+            console.log(msg.code + " : " + msg.massage);
+            return res.send({ err: msg.code + " : " + msg.massage });
+        } else {
+            // 유저 DIV 값으로 DB에서 정보 읽어오기
+            console.log(msg.userDIV);
+            query(msg.userDIV);
+        }
+    } else {
+        console.log("토큰 없음");
+        query(msg.userDIV);
+    }
+})
+
 /* 다른 유저 ID 조회 comment 반환 */
 router.get('/:id', wrapper(async function (req, res) {
-        userDIV = req.params.id;
-        const userComment = await db.anotherPage(userDIV);
-        return res.send(userComment[0]);
-    }
+    userDIV = req.params.id;
+    const userComment = await db.anotherPage(userDIV);
+    return res.send(userComment[0]);
+}
 ));
 
 /* 다른 유저 ID 조회 프로필 사진, 닉네임, 가입 날짜 반환 */
 router.get('/:id/profile', wrapper(async function (req, res) {
     userDIV = req.params.id;
+    
     const userComment = await db.anotherProfile(userDIV);
     return res.send(userComment[0]);
 }
@@ -24,19 +47,27 @@ router.get('/:id/profile', wrapper(async function (req, res) {
 
 /* 다른 유저 ID 조회 올린 프로젝트 정보 반환 */
 router.get('/:id/upload', wrapper(async function (req, res) {
-        userDIV = req.params.id;
-        const upLoadProject = await db.anotherUploadProject(userDIV);
+    userDIV = req.params.id;
+
+    const query = async function (myDIV) {
+        const upLoadProject = await db.anotherUploadProject(userDIV,myDIV);
         return res.send(upLoadProject);
     }
-));
+
+    readHeart(req, res, query);
+}));
 
 /* 다른 유저 ID 조회 구매한 프로젝트 정보 반환 */
 router.get('/:id/buy', wrapper(async function (req, res) {
-        userDIV = req.params.id;
-        const buyProject = await db.anotherBuyProject(userDIV);
+    userDIV = req.params.id;
+
+    const query = async function (myDIV) {
+        const buyProject = await db.anotherBuyProject(userDIV,myDIV);
         return res.send(buyProject);
     }
-));
+
+    readHeart(req, res, query);
+}));
 
 /* 다른 유저 ID 조회 올린 프로젝트 정보 반환 */
 router.get('/:id/uploadcount', wrapper(async function (req, res) {
--- routes/user/myPage.js
+++ routes/user/myPage.js
@@ -5,112 +5,98 @@ const middle = require('../../middleware/userMiddleWare');
 const wrap = require('../../util/wrapper');
 const wrapper = wrap.wrapper;
 
-/* mypage 조회 comment 반환 */
-router.get('/', wrapper(async function (req, res) {
+// 찜 목록 나타내는 부분 모듈화 처리
+const readHeart = wrapper(async function (req, res, query) {
     // 토큰 가져오기
     const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
+    if (toke !== undefined) {
+        // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
+        const msg = await middle.verifyToken(toke);
+        // 에러 발생 시 res로 에러 반환
+        if (msg.code) {
+            console.log(msg.code + " : " + msg.massage);
+            return res.send({ err: msg.code + " : " + msg.massage });
+        } else {
+            // 유저 DIV 값으로 DB에서 정보 읽어오기
+            console.log(msg.userDIV);
+            query(msg.userDIV);
+        }
     } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const userComment = await db.myPageComment(msg.userDIV);
+        console.log("토큰 없음");
+        query(msg.userDIV);
+    }
+})
+
+/* mypage 조회 comment 반환 */
+router.get('/', wrapper(async function (req, res) {
+    
+    const query = async function (userDIV) {
+        const userComment = await db.myPageComment(userDIV);
         return res.send(userComment[0]);
     }
+
+    readHeart(req, res, query);
+
 }));
 
 /* mypage 조회 닉네임, 사진 , 가입한 날짜 정보 반환 */
 router.get('/profile', wrapper(async function (req, res) {
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const userComment = await db.myProfile(msg.userDIV);
+
+    const query = async function (userDIV) {
+        const userComment = await db.myProfile(userDIV);
         return res.send(userComment[0]);
     }
+
+    readHeart(req, res, query);
+
 }));
 
 /* mypage 조회 올린 프로젝트 정보 반환 */
 router.get('/upload', wrapper(async function (req, res) {
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const upLoadProject = await db.myUploadProject(msg.userDIV);
+
+    const query = async function (userDIV) {
+        const upLoadProject = await db.myUploadProject(userDIV);
         return res.send(upLoadProject);
     }
+
+    readHeart(req, res, query);
+
 }));
 
 /* mypage 조회 올린 프로젝트 수 반환 */
 router.get('/uploadcount', wrapper(async function (req, res) {
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const upLoadCount = await db.myUploadCount(msg.userDIV);
+
+    const query = async function (userDIV) {
+        const upLoadCount = await db.myUploadCount(userDIV);
         return res.send(upLoadCount[0]);
     }
+
+    readHeart(req, res, query);
+
 }));
 
 /* mypage 조회 구매한 프로젝트 정보 반환 */
 router.get('/buy', wrapper(async function (req, res) {
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const buyProject = await db.myBuyProject(msg.userDIV);
+
+    const query = async function (userDIV) {
+        const buyProject = await db.myBuyProject(userDIV);
         return res.send(buyProject);
     }
+
+    readHeart(req, res, query);
+
 }));
 
 /* mypage 구매한 프로젝트 수 반환 */
 router.get('/buycount', wrapper(async function (req, res) {
-    // 토큰 가져오기
-    const toke = req.get('user_token');
-    // 토큰 가져온 후 검증 - 에러 코드 or 유저 판별 DIV 반환
-    const msg = await middle.verifyToken(toke);
-    // 에러 발생 시 res로 에러 반환
-    if (msg.code) {
-        console.log(msg.code + " : " + msg.massage);
-        return res.send({ err: msg.code + " : " + msg.massage });
-    } else {
-        // 유저 DIV 값으로 DB에서 정보 읽어오기
-        console.log(msg.userDIV);
-        const upLoadCount = await db.myBuyCount(msg.userDIV);
+
+    const query = async function (userDIV) {
+        const upLoadCount = await db.myBuyCount(userDIV);
         return res.send(upLoadCount[0]);
     }
+    readHeart(req, res, query);
+
+
 }));
 
 module.exports = router;
