bff3962398d3ed7773eb380bbbdec0e52b83ba45
Newt-JH <flozzang@naver.com>
2022-08-16 18:13:27 +0900

    코드 줄맞춤

--- routes/user/anotherUserProfile.js
+++ routes/user/anotherUserProfile.js
@@ -21,9 +21,9 @@ router.get('/:id/profile', wrapper(async function (req, res) {
     const upLoadCount = await anotherdb.myUploadCount(userID);
     const buyCount = await anotherdb.myBuyCount(userID);
     const resData = {
-        userProfile : userProfile[0],
-        upLoadCount : upLoadCount[0],
-        buyCount    : buyCount[0]
+        userProfile: userProfile[0],
+        upLoadCount: upLoadCount[0],
+        buyCount: buyCount[0]
     }
     return res.send(resData);
 }
