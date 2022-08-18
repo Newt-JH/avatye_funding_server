1512d8a8399a1b023d611fabbbdceb3e3f40e067
limjeahyuk <dlawo9368@email.daelim.ac.kr>
2022-08-18 17:46:07 +0900

    create project 서버 진짜 끝

--- DB/procedure/createProject.sql
+++ DB/procedure/createProject.sql
@@ -1,7 +1,7 @@
 drop procedure createProject;
 
 DELIMITER $$
-create procedure createProject(IN categoryindex varchar(200),userDIV varchar(20),longTi varchar(30),shortTi varchar(20),susummary varchar(100),imgUrl varchar(50),serch varchar(30), content text, startData varchar(100), theEndDate varchar(100), ggoalprice Integer)
+create procedure createProject(IN categoryindex varchar(200),userDIV varchar(20),longTi varchar(30),shortTi varchar(20),susummary varchar(100),imgUrl varchar(200),serch varchar(30), content text, startData varchar(100), theEndDate varchar(100), ggoalprice Integer)
 begin
         insert into
     project(cateIndex,userID,longTitle,shortTitle,summary,profileIMG,searchTag, contents, beginDate, endDate, goalPrice)
