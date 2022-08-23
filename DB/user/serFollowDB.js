6231d329a6e5c5ddd76152fafec1fff50fcda5ac
Newt-JH <flozzang@naver.com>
2022-08-23 15:27:22 +0900

    follow 끝

--- DB/user/serFollowDB.js
+++ DB/user/serFollowDB.js
@@ -15,14 +15,14 @@ function follow(userID, followedID) {
 
 // 팔로워 리스트
 function followerList(userID) {
-    const query = `select following from follow where followed = '${userID}' and followingCheck = 1;`
+    const query = `call followerList('${userID}');`
 
     return conpro(query);
 }
 
 // 팔로우 한 리스트
 function followingList(userID) {
-    const query = `select followed from follow where following = '${userID}' and followingCheck = 1;`
+    const query = `call followed('${userID}');`
 
     return conpro(query);
 }
