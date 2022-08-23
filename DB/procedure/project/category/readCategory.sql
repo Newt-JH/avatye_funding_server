8e21db3995be283e210fb1d459c3535beef86a8e
Newt-JH <flozzang@naver.com>
2022-08-23 15:50:14 +0900

    category 끝

---
+++ DB/procedure/project/category/readCategory.sql
@@ -0,0 +1,13 @@
+drop procedure readCategory;
+
+DELIMITER $$
+create procedure readCategory(IN inputUser varchar(50))
+begin
+    select (p.nowPrice/p.goalPrice * 100) as percent,p.projectIndex, longTitle,summary,
+        profileIMG, goalPrice,nowPrice,endDate,nickName,c.name,uP.userID,(select heartCheck from heart where userID = inputUser and projectIndex = p.projectIndex) as heartCheck
+        from project p
+            join category c
+                on p.cateIndex = c.cateIndex
+            join userProfile uP
+                on p.userID = uP.userID;
+end $$
--- routes/project/category.js
+++ routes/project/category.js
@@ -4,22 +4,13 @@ const db = require('../../DB/project/serCategoryDB');
 const wrap = require('../../util/wrapper');
 const wrapper = wrap.wrapper;
 
-
-// 메인 화면 주목할만한 프로젝트
-router.get('/', wrapper(async function(req, res) {
-    
-    let f = await db.readAll();
-    res.send(f);
-    
-}));
-
 // 전체 상품 읽어오기
 router.get('/all', wrapper(async function (req, res) {
     const userID = req.userID;
     console.log(userID);
     let f = await db.readCategory(userID);
 
-    res.send(f);
+    res.send(f[0]);
 }));
 
 // 카테고리별 상품 읽어오기
@@ -28,7 +19,7 @@ router.get('/:id', wrapper(async function (req, res) {
     const cateName = decodeURIComponent(req.params.id);
     let f = await db.oneCategory(cateName,userID);
 
-    res.send(f);
+    res.send(f[0]);
 }));
 
 module.exports = router;
