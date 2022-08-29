24ddcdcd9865df334ab22c30f0a90367624a5a54
Newt-JH <flozzang@naver.com>
2022-08-29 17:36:18 +0900

    lasy review

--- DB/procedure/order/orderPaymentData.sql
+++ DB/procedure/order/orderPaymentData.sql
@@ -3,5 +3,5 @@ drop procedure orderPaymentData;
 DELIMITER $$
 create procedure orderPaymentData(IN inUserID varchar(50))
 begin
-    select * from payment where userID = 'vx51zp9me0' and paymentCheck = 1;
+    select * from payment where userID = inUserID and paymentCheck = 1;
 end $$
